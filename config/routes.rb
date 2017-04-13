4a228779cfe8fb402f5fb6cf3c01bdced776613c
AndrewThian <andrewbobbythian@gmail.com>
2017-04-13 10:54:22 +0800

    integrate index action for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :portfolios
   get 'pages/home'
 
   get 'pages/about'
@@ -7,5 +6,6 @@ Rails.application.routes.draw do
   get 'pages/contact'
 
   resources :blogs
+  resources :portfolios
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
