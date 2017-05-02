5d8103d03b0e4ce307d700c6c8531188399c2327
AndrewThian <andrewbobbythian@gmail.com>
2017-05-02 09:52:14 +0800

    impl default portfolio image

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,13 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails, -> { where(subtitle: "Angular") }
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
